9c145b4c11a85880a2fd0107a7b6e535c2af0d9c
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2016-09-19 08:39:02 +0200

    [chore] Add tests for MySQL and PostgreSQL databases

--- src/test/java/org/springframework/data/jpa/datatables/model/User.java
+++ src/test/java/org/springframework/data/jpa/datatables/model/User.java
@@ -7,8 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
 
   @Id
