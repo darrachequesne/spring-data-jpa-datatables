09dca46eb8db6ff79185463f84b98594625bfaac
vchernogorov <vchernogorov@apple.com>
2021-10-07 14:02:12 +0300

    Add NoArgConstructor to SearchBuilder

--- src/main/java/org/springframework/data/jpa/datatables/mapping/DataTablesInput.java
+++ src/main/java/org/springframework/data/jpa/datatables/mapping/DataTablesInput.java
@@ -1,12 +1,17 @@
 package org.springframework.data.jpa.datatables.mapping;
 
-import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
--- src/main/java/org/springframework/data/jpa/datatables/mapping/SearchBuilder.java
+++ src/main/java/org/springframework/data/jpa/datatables/mapping/SearchBuilder.java
@@ -2,6 +2,7 @@ package org.springframework.data.jpa.datatables.mapping;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
@@ -10,12 +11,14 @@ import java.util.Map;
  * <a href="https://datatables.net/extensions/searchbuilder/predefined">Predefined Searches</a>
  */
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class SearchBuilder {
   private List<Criteria> criteria;
   private String logic;
 
   @Data
+  @NoArgsConstructor
   @AllArgsConstructor
   public static class Criteria {
     private String condition;
